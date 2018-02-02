d5d711c9a125bb58704663e2692bf8114b184d00
Aaron Gaspar Peña <gaspar_aaron@hotmail.com>
2018-02-02 13:03:45 -0500

    Servicio de firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -8,16 +8,32 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
+  equipo:any[] = [];
 
   constructor( public http:Http )
   {
-    this.http.get("assets/data/infopagina.json")
-    .subscribe(data=>{
-      console.log(data.json());
-      this.cargada= true;
-      this.info=data.json();
-    })
-    //data.json() solo el archivo json
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+   public carga_info()
+   {
+     this.http.get("assets/data/infopagina.json")
+     .subscribe(data=>{
+       //console.log(data.json());
+       this.cargada= true;
+       this.info=data.json();
+     })
+     //data.json() solo el archivo json
+   }
+   public carga_sobre_nosotros()
+   {
+     this.http.get("https://about-2ad57.firebaseio.com/Nosotros.json")
+     .subscribe(data=>{
+       console.log(data.json());
+       this.cargada= true;
+       this.info=data.json();
+     })
    }
 
 }
