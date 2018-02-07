9109bb67b3fe906fb48d0ea31bbfb16aeb6a91ac
Aaron Gaspar Peña <gaspar_aaron@hotmail.com>
2018-02-07 17:21:16 -0500

    Loading Agregado

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -20,8 +20,13 @@ export class ProductosService
       this.http.get("https://about-2ad57.firebaseio.com/producto_idx.json")
       .subscribe(res=>{
         console.log(res.json());
-        this.cargando = false;
-        this.productos=res.json();
+        //setTimeout funcion para ver la animacion cargando en el portafolio.component.html
+        //setTimeout(()=>
+        //{
+          this.cargando = false;
+          this.productos=res.json();
+        //},1500)
+
       })
     }
   }
